8076eb65366860f46951ac89206b657e2ae24ef2
Darkboy78778 <amirhamza78778@gmail.com>
2026-08-25 22:53:48 +0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/lovelettergf">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/create" element={<CreateGift />} />
